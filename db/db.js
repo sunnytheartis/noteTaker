3fd1ba1413f9b1efc4d671698cc7b98d0cb133cd
Sun <solskinartwork@gmail.com>
2021-11-19 18:43:54 -0500

    adding db.js

---
+++ db/db.js
@@ -0,0 +1,39 @@
+const util = require('util');
+const fs = require('fs');
+const uuidv1 = require('uuid/v1');
+
+const read = util.promisify(fs.readFile);
+const write = util.promisify(fs.writeFile);
+
+class Db {
+  read() {
+    return read('db/db.json', 'utf8');
+  }
+
+  write(note) {
+    return write('db/db.json', JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then((notes) => {
+      let parsedNotes = [].concat(JSON.parse(notes));
+
+      return parsedNotes ;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+    
+    const newNote = { title, text, id: uuidv1() };
+
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
+}
